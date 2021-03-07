802bb03f43908f5f16c51e556cbeb784aec0953c
Faduma Elmi <fowziiae@gmail.com>
2021-03-07 12:02:37 -0600

    set up to render details on DOM

--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -1,4 +1,17 @@
+import { useSelector } from 'react-redux';
+
+// will useSelector to get single movie detail from store that i havent set up yet
+
+// will render the details on the DOM
+
 function DetailsPage() {
-  return <div>Details page</div>;
+  const movieInfo = useSelector((store) => store.movieDetails);
+  return (
+    <>
+      <h3>{movieInfo[0].title}</h3>
+      <img src={movieInfo[0].poster}></img>
+      <p>{movieInfo[0].description}</p>
+    </>
+  );
 }
 export default DetailsPage;
