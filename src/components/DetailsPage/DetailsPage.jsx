d2fad28be5eba1b2fc60e3f9fa68c524b310d352
Faduma Elmi <fowziiae@gmail.com>
2021-03-07 16:58:05 -0600

    added back button that takes you back to the home page

--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
+import { useHistory } from 'react-router-dom';
 // will useSelector to get single movie detail from store that i havent set up yet
 
 // will render the details on the DOM
 
 function DetailsPage() {
+  //gets movie details from store
   const movieInfo = useSelector((store) => store.movieDetails);
-
+  const history = useHistory();
   console.log('details', movieInfo);
 
   //now i want to add the genre
+  //gets genres from store
   const genres = useSelector((store) => store.genres);
   return (
     <>
@@ -23,6 +25,7 @@ function DetailsPage() {
         {genres.map((genre) => {
           return <p key={genre.name}>{genre.name}</p>;
         })}
+        <button onClick={() => history.push('/')}>Back</button>
       </div>
     </>
   );
