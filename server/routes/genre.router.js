d80549558c00bb1cddc9601613204f400acf0a1f
Faduma Elmi <fowziiae@gmail.com>
2021-03-07 01:16:02 -0600

    added GET route for movie details and for genre

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../modules/pool')
+const pool = require('../modules/pool');
 
 router.get('/', (req, res) => {
   // Add query to get all genres
-  res.sendStatus(500)
+  const sqlText = `SELECT * FROM "genres"`;
+  pool
+    .query(sqlText)
+    .then((response) => {
+      res.send(response.rows);
+    })
+    .catch((error) => {
+      console.log('Error on get genres', error);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
