cdba6609f8d73b1415d0e91db165a46be6282163
Faduma Elmi <fowziiae@gmail.com>
2021-03-07 14:20:14 -0600

    created handle to switch to details page when the movie is clicked

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,32 +1,40 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import './MovieList.css'
-
+import './MovieList.css';
+import { useHistory } from 'react-router-dom';
 function MovieList() {
+  const dispatch = useDispatch();
+  const movies = useSelector((store) => store.movies);
+  const history = useHistory();
+  useEffect(() => {
+    dispatch({ type: 'FETCH_MOVIES' });
+  }, []);
 
-    const dispatch = useDispatch();
-    const movies = useSelector(store => store.movies);
+  // create handle to switch to details page when movie is clicked and calls detail
+  const showDetails = (thisMovie) => {
+    console.log('in show details', thisMovie);
 
-    useEffect(() => {
-        dispatch({ type: 'FETCH_MOVIES' });
-    }, []);
-
-    return (
-        <main>
-            <h1>MovieList</h1>
-            <section className="movies">
-                {movies.map(movie => {
-                    return (
-                        <div key={movie.id} >
-                            <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
-                        </div>
-                    );
-                })}
-            </section>
-        </main>
-
-    );
+    dispatch({
+      type: 'GET_DETAILS',
+      payload: thisMovie,
+    });
+    history.push('/details');
+  };
+  return (
+    <main>
+      <h1>MovieList</h1>
+      <section className="movies">
+        {movies.map((movie) => {
+          return (
+            <div key={movie.id} onClick={() => showDetails(movie.id)}>
+              <h3>{movie.title}</h3>
+              <img src={movie.poster} alt={movie.title} />
+            </div>
+          );
+        })}
+      </section>
+    </main>
+  );
 }
 
 export default MovieList;
